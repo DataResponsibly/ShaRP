d85629e3dce5bb4de7b669222b7e43d796a298c7
joaopfonseca <jpmrfonseca@gmail.com>
2023-11-28 11:38:46 +0000

    FIX bug regarding pairwise analysis of ranking-based QoIs (#4)

--- sharp/base.py
+++ sharp/base.py
@@ -78,33 +78,29 @@ class ShaRP(BaseEstimator):
         self._X = kwargs["X"] if "X" in kwargs.keys() else None
         self._y = kwargs["y"] if "y" in kwargs.keys() else None
 
-    def _check_params(self, X, y):  # TODO: refactor _check_params
-        if X is None:
-            X = self._X
-
-        if y is None:
-            y = self._y
-
-        feature_names = check_feature_names(X)
-
+    def fit(self, X, y=None):
         X_, y_ = check_inputs(X, y)
-        measure_ = check_measure(self.measure)
-        qoi_ = check_qoi(
-            self.qoi,
-            target_function=self.target_function,
-            X=X_,
-        )
 
-        if not hasattr(self, "_rng"):
-            self._rng = check_random_state(self.random_state)
+        self._rng = check_random_state(self.random_state)
 
-        return feature_names, X_, y_, measure_, qoi_
+        if isinstance(self.qoi, str):
+            self.qoi_ = check_qoi(
+                self.qoi,
+                target_function=self.target_function,
+                X=X_,
+            )
+        else:
+            self.qoi_ = self.qoi
+
+        self.feature_names_ = check_feature_names(X)
+
+        self.measure_ = check_measure(self.measure)
 
     def individual(self, sample, X=None, y=None, **kwargs):
         """
         set_cols_idx should be passed in kwargs if measure is marginal
         """
-        feature_names, X_, y_, measure_, qoi_ = self._check_params(X, y)
+        X_, y_ = check_inputs(X, y)
 
         if "set_cols_idx" in kwargs.keys():
             set_cols_idx = kwargs["set_cols_idx"]
@@ -115,13 +111,13 @@ class ShaRP(BaseEstimator):
             sample = X_[sample]
 
         influences = []
-        for col_idx in range(len(feature_names)):
-            cell_influence = measure_(
+        for col_idx in range(len(self.feature_names_)):
+            cell_influence = self.measure_(
                 row=sample,
                 col_idx=col_idx,
                 set_cols_idx=set_cols_idx,
                 X=X_,
-                qoi=qoi_,
+                qoi=self.qoi_,
                 sample_size=self.sample_size,
                 rng=self._rng,
             )
@@ -132,8 +128,11 @@ class ShaRP(BaseEstimator):
         """
         set_cols_idx should be passed in kwargs if measure is marginal
         """
-        feature_names, X_, y_, measure_, qoi_ = self._check_params(X, y)
-        col_idx = feature_names.index(feature) if type(feature) is str else feature
+        X_, y_ = check_inputs(X, y)
+
+        col_idx = (
+            self.feature_names_.index(feature) if type(feature) is str else feature
+        )
 
         if "set_cols_idx" in kwargs.keys():
             set_cols_idx = kwargs["set_cols_idx"]
@@ -143,12 +142,12 @@ class ShaRP(BaseEstimator):
         influences = []
         for sample_idx in range(X_.shape[0]):
             sample = X_[sample_idx]
-            cell_influence = measure_(
+            cell_influence = self.measure_(
                 row=sample,
                 col_idx=col_idx,
                 set_cols_idx=set_cols_idx,
                 X=X_,
-                qoi=qoi_,
+                qoi=self.qoi_,
                 sample_size=self.sample_size,
                 rng=self._rng,
             )
@@ -172,6 +171,8 @@ class ShaRP(BaseEstimator):
     def pairwise(self, sample1, sample2, **kwargs):
         """
         Compare two samples, or one sample against a set of samples.
+        If `sample1` or `sample2` are of type `int` or `list`, `X` also needs to be
+        passed.
 
         set_cols_idx should be passed in kwargs if measure is marginal
         """
