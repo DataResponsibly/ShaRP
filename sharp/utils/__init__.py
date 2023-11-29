c239bf5cd5d64e40f415b75612ba120db88f7cfa
joaopfonseca <jpmrfonseca@gmail.com>
2023-11-29 14:18:16 +0000

    ENH change default parameter values (#11)

--- sharp/utils/__init__.py
+++ sharp/utils/__init__.py
@@ -1,4 +1,4 @@
-from ._utils import check_feature_names, check_inputs, check_measure, check_qoi
+from ._checks import check_feature_names, check_inputs, check_measure, check_qoi
 from ._rank_utils import scores_to_rank
 
 __all__ = [
