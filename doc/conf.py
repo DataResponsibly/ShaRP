93cb9076b774e7d17d17747371244dae33e4b8bd
joaopfonseca <jpmrfonseca@gmail.com>
2023-12-12 20:53:01 +0000

    STYLE code formatting (#5)

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ShaRP'
-copyright = '2023, Joao Fonseca and Venetia Pliatsika'
-author = 'Joao Fonseca and Venetia Pliatsika'
+project = "ShaRP"
+copyright = "2023, Joao Fonseca and Venetia Pliatsika"
+author = "Joao Fonseca and Venetia Pliatsika"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -33,20 +33,20 @@ autosummary_generate = True
 autoclass_content = "class"
 autodoc_default_flags = ["members", "inherited-members"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # Generate the plots for the gallery
-plot_gallery = 'True'
+plot_gallery = "True"
 
 sphinx_gallery_conf = {
-     'examples_dirs': '../examples',   # path to your example scripts
-     'gallery_dirs': 'auto_examples',  # path to where to save gallery generated output
+    "examples_dirs": "../examples",  # path to your example scripts
+    "gallery_dirs": "auto_examples",  # path to where to save gallery generated output
 }
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_permalinks_icon = '<span>#</span>'
-html_theme = 'sphinxawesome_theme'
-html_static_path = ['_static']
+html_theme = "sphinxawesome_theme"
+html_static_path = ["_static"]
